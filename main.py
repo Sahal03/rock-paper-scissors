100fbe3a7425ff35583d66778867d62bed2015ad
Sahal A <96517902+Sahal03@users.noreply.github.com>
2022-01-02 02:59:54 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import random,time
+
+BOT = ['ROCK','PAPER','SCISSORS']
+wins = 0
+
+def game(num):
+    wins = num
+
+    y = random.choice(BOT)
+    x = input('Rock, Paper or Scissors:\n')
+    time.sleep(0.5)
+    print("The computer played",y)
+    if x.upper() == y:
+        print("It's a tie!")
+    elif x.upper() == 'SCISSORS':
+        if y.upper() == 'PAPER':
+            print("You win!")
+            wins+=1
+        else:
+            print("You lose!")
+    elif x.upper() == 'ROCK':
+        if y.upper() == 'SCISSORS':
+            print('You win!')
+            wins+=1
+        else:
+            print('You lose!')
+    else:
+        if y.upper() == 'ROCK':
+            print('You win!')
+            wins+=1
+        else:
+            print('You lose!')
+    print("You have",wins,"win(s)!")
+    retry(wins)
+
+def retry(num):
+    x = input('Would you like to play again? (YES or NO):\n')
+    if x.upper() == 'YES':
+        game(num)
+    else:
+        print('Have a nice day!')
+
+def main():
+    game(wins)
+
+main()
