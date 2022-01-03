26405b2134c74c95342cf523ae6e7e5ba05efead
Sahal A <sahalaidid@gmail.com>
2022-01-03 00:40:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import random,time
 
+#list of options for the computer
 BOT = ['ROCK','PAPER','SCISSORS']
+#wins variable
 wins = 0
 
+#pass a variable to keep track of wins
 def game(num):
+
+#wins variable
     wins = num
 
+#computer plays
     y = random.choice(BOT)
+#user input
     x = input('Rock, Paper or Scissors:\n')
+
+#small buffer for clarity
     time.sleep(0.5)
-    print("Player 2 played",y)
+    
+#nested control structers to determine winner  
     if x.upper() == y:
         print("It's a tie!")
     elif x.upper() == 'SCISSORS':
@@ -24,22 +34,33 @@ def game(num):
             wins+=1
         else:
             print('You lose!')
-    else:
+    elif x.upper() == 'PAPER':
         if y.upper() == 'ROCK':
             print('You win!')
             wins+=1
         else:
             print('You lose!')
-    print("You have",wins,"win(s)!")
+    else:
+        print("Try Entering a Valid Option")
+        game(wins)
+
+
+    print("\nPlayer 2 played",y)
+    print("\nYou have",wins,"win(s)!")
+    
+#call retry function
     retry(wins)
 
+#retry function carries the win variable
 def retry(num):
     x = input('Would you like to play again? (YES or NO):\n')
     if x.upper() == 'YES':
         game(num)
     else:
         print('Have a nice day!')
+        quit()
 
+#main
 def main():
     game(wins)
 
